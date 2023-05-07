71e749fcffc3379dfa30a11ca784f350f9e858a3
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-07 16:24:18 +0530

    02_Assignment TypeOf
    02_Assignment Type of

---
+++ 03_typeOf.js
@@ -0,0 +1,48 @@
+
+console.log("/--------Before Initialization----------/");
+
+var bankName
+console.log("Bank Name ==>", bankName);
+var typeOfBankName =typeof bankName;
+console.log("Type Of Bank Name ==>",typeOfBankName);
+
+console.log("");
+console.log("/--------After Initialization----------/");
+var bankName = "SBI Bank";
+console.log("Bank Name ==>", bankName);
+var typeOfBankName =typeof bankName;
+console.log("Type Of Bank Name ==>",typeOfBankName);
+
+console.log("");
+console.log("/----------------Marks----------------/");
+var marks= '90%' ;
+var typeofMarks =typeof marks;
+console.log("Marks == >",marks ,"Type of Marks ==>", typeofMarks);
+
+console.log("");
+console.log("/--------Before Initialization----------/");
+var isWorking 
+console.log("Before Initialization value ==>", isWorking);
+
+console.log("");
+console.log("/--------After Initialization----------/");
+var isWorking ="true";
+console.log("After Initialization Value ==>", isWorking);
+
+console.log("");
+console.log("/--------Before Update Value----------/");
+var totalCount ="Hundred and Seven";
+var typeOfTotalCount=typeof totalCount;
+console.log("Total Count is ==> ",totalCount )
+console.log("Type of Total Count is ==>",typeOfTotalCount );
+
+console.log("");
+console.log("/--------After Update Value----------/");
+var totalCount = 107;
+var typeOfTotalCount=typeof totalCount;
+console.log("Total Count is ==> ",totalCount )
+console.log("Type of Total Count is ==>",typeOfTotalCount );
+
+
+
+
