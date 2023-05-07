89c7c2cd7257df843e2d5fe163201354065f4078
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-07 08:32:13 +0530

    variable assign
    variable assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,19 @@
+var myFullName ;   
+console.log(myFullName);        //Decalre variable with name as myFullName
+myFullName="Ajay Patil"         //Log variable 
+console.log(myFullName);        //Initializing 
+
+myFullName="Ajay Ramesh Patil";
+console.log(myFullName);
+
+var myLove ="I love only JS";
+console.log(myLove);
+
+var firstName ="Ajay";
+var lastName ="Patil";
+var age ="26";
+var address ="At.Post Dhamodi Tal: Raver Dist: Jalgaon";
+var pincode ="425507";
+var gender ="Male";
+var isMarried = "unmarried";
+console.log(firstName, lastName, age,address, pincode, gender,isMarried )
