7bf76a9879bbd303abe4cf5e326719214498283d
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-12 10:44:38 +0530

    Update file
    Update file

--- 03_functions.js
+++ 03_functions.js
@@ -7,3 +7,7 @@ var returnValue = addition(100, 200);
 console.log("Addition of number is: ", returnValue);
 var returnResult = addition(890.400, 600.3456);
 console.log("Addition of number is: ", returnResult);
+
+var firstName="Ajay";
+var result=firstName.length;
+console.log(" Total Number of charachter in given string =>",result);
