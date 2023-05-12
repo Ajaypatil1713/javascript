0981e2d8e58412cc585fe48972ce53f4460090b1
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-12 07:53:00 +0530

    Swap Variables Assignment
    Swap Variables Assignment

---
+++ swapVariableAssignment.js
@@ -0,0 +1,20 @@
+var num1 =100;
+var num2 = 200;
+var num3 = 300;
+var temp;
+
+
+// After swapping 
+console.log("/----------------------After Swapping---------------------------/");
+console.log("Num1 value =>",num1, "Num2 value =>",num2,"Num3 value =>", num3)
+console.log("/---------------------------------------------------------------/");
+
+//After Swapping
+temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+
+console.log("/----------------------Before Swapping---------------------------/");
+console.log("Num1 value =>",num1, "Num2 value =>",num2,"Num3 value =>", num3)
+console.log("/----------------------------------------------------------------/");
