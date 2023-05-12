4601e08588f6b0802a14e64815d0e3d5de4f776f
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-12 07:45:51 +0530

    Create Swap Variables
    Create Swap Variables

---
+++ 02_swapVariables.js
@@ -0,0 +1,40 @@
+
+console.log("/-------------------Before Swap-----------------/");
+console.log("/-----------------------------------------------/");
+var sweety = "Sweety";
+var cutie = "Cutie";
+
+console.log("");
+console.log("sweety =", sweety);
+console.log("cutie =", cutie);
+
+// Swapping the values
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+
+console.log("");
+console.log("***********************************************")
+console.log("/-------------------After Swap-----------------/");
+console.log("/-----------------------------------------------/");
+console.log("sweety =", sweety);
+console.log("cutie =", cutie);
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+
+console.log("");
+console.log("/-----------------------------------------------/");
+console.log("/-------------------Before Swap-----------------/");
+console.log("num1: ", num1, " num2: ", num2, "num3: ", num3);
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("");
+console.log("/-----------------------------------------------/");
+console.log("/-------------------After Swap-----------------/");
+console.log("num1:", num1, " num2:", num2, "num3: ", num3);
+console.log("/-----------------------------------------------/");
+console.log("/------------------------*-----------------------/");
