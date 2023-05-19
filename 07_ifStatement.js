73b125a1fc0ab6e5c448ce37f03fba28f96645db
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-19 14:11:17 +0530

    remove duplicate code
    remove duplicate code

--- 07_ifStatement.js
+++ 07_ifStatement.js
@@ -14,7 +14,7 @@ if(isEven){
     console.log(`Is Odd ${isEven}, Odd Number : ${num1}`);
 }
 
-
+//--------------------------------------------------------------------------------------------------
 // console.log("I am going to office");
 
 // var isTrafficToDailyRoute = false;
@@ -23,7 +23,7 @@ if(isEven){
 // }
     // console.log("Going by Cab");
     // console.log("Had breakfast in middle");
-
+//--------------------------------------------------------------------------------------------------
 // var num1 = 13; 
 // var isEven = num1%2==0;// false
 // if (isEven) {  
@@ -37,30 +37,30 @@ if(isEven){
 // gender - female --> age >=18   AND <=90
 
 // gender - transgender --> invalid
-
-function marriageEligibility(age, gender, name){
-    if (age==null || age==undefined || gender==null || gender== undefined ) {
-       console.log(`Invalid Input  ${name}  Age: ${age}  Gender: ${gender}` ); 
-    } else {
-       if ((gender == "male" && age <21 || age >90) ) {
-          console.log(`Invalid Input ${name} Age: ${age}  Gender:${gender}` ); 
-       } else {
-            if (gender=="female" && age <18 || age > 90) {
-                console.log(`Invalid Input  ${name} ${age} ${gender}` ); 
-            } else {
-                 if (gender=="transgender") {
-                    console.log(`Not allowed ${name} ${age} ${gender} as constitution` ); 
-                 } else { 
-                    if (gender=="male") {
-                        console.log(`You are eligible ${name} ${age} ${gender} `);
-                    } else {
-                        console.log(`You are eligible ${name} ${age} ${gender} `);
-                    }
-                 }
-            }
-       }  
-    }
-}
+//--------------------------------------------------------------------------------------------------
+// function marriageEligibility(age, gender, name){
+//     if (age==null || age==undefined || gender==null || gender== undefined ) {
+//        console.log(`Invalid Input  ${name}  Age: ${age}  Gender: ${gender}` ); 
+//     } else {
+//        if ((gender == "male" && age <21 || age >90) ) {
+//           console.log(`Invalid Input ${name} Age: ${age}  Gender:${gender}` ); 
+//        } else {
+//             if (gender=="female" && age <18 || age > 90) {
+//                 console.log(`Invalid Input  ${name} ${age} ${gender}` ); 
+//             } else {
+//                  if (gender=="transgender") {
+//                     console.log(`Not allowed ${name} ${age} ${gender} as constitution` ); 
+//                  } else { 
+//                     if (gender=="male") {
+//                         console.log(`You are eligible ${name} ${age} ${gender} `);
+//                     } else {
+//                         console.log(`You are eligible ${name} ${age} ${gender} `);
+//                     }
+//                  }
+//             }
+//        }  
+//     }
+// }
 // marriageEligibility(null, "male", "Shyam");
 // marriageEligibility(undefined, "male", "Shyam");
 // marriageEligibility(31, null, "Shyam");
@@ -73,26 +73,5 @@ function marriageEligibility(age, gender, name){
 // marriageEligibility(98, "female", "Jenny");
 
 
-marriageEligibility(23, "female", "Menny");
-marriageEligibility(27, "male", "Bill");
-
-
-
-
-
-// marriageEligibility(23, "male", "Shyam");
-// marriageEligibility(21, "female", "Jenny");
-// marriageEligibility(33, "transgender", "Matoki");
-
-// marriageEligibility(16, "female", "Ketty"); 
-// marriageEligibility(20, "male", "Sandy"); 
-
-// marriageEligibility(350, "male", "Mandy"); 
-
-// marriageEligibility(-30, "male", "Mandy"); 
-
-// marriageEligibility(null, "male", "Mandy"); 
-
-// marriageEligibility(-30, NaN, "Mandy");
-
-// marriageEligibility(42, null, "Mandy");
+// marriageEligibility(23, "female", "Menny");
+// marriageEligibility(27, "male", "Bill");
