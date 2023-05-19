f75e5b82e0a297af9887f9e19358391aa55634ff
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-19 14:06:29 +0530

    if else grade system assignment
    if else grade system assignment

--- 06_isElse_grade.js
+++ 06_isElse_grade.js
@@ -26,3 +26,46 @@ voteEligibility(0);
 voteEligibility(null);
 voteEligibility();
 console.log("----------------------------------------------------------------------------------------------");
+
+// function gradeCalculation(marks){
+//     if (marks <= 0 || marks > 100 || isNaN(marks)) {
+//             console.log(`Please provide the valid marks`);
+//     } else {
+//         if (marks == undefined || marks == null) {
+//             console.log(`Please provide the valid marks`);
+//         } else {
+//             if (marks >= 90) {
+//                 console.log(`Fantastic Marks ${marks}, Your Grade is A+`);
+//             } else {
+//                 if (marks >=75 ) {
+//                     console.log(`Fantastic Marks ${marks}, Your Grade is A`);
+//                 } else {
+//                     if (marks >= 50 ) {
+//                             console.log(`Excellent Marks ${marks}, Your Grade is B`);
+//                     } else {
+//                         if (marks >= 35 ) {
+//                             console.log(`Marks is ${marks}, your grade is C, Need improvement.`);
+//                         } else {
+//                             console.log(`Your Marks ${marks} very less, hence you are Failed... `);
+//                         }                        
+//                     }
+//                 }                
+//             }
+//         }
+//     }
+// }
+// gradeCalculation(98);
+// gradeCalculation(80);
+// gradeCalculation(90);
+// gradeCalculation(0);
+// gradeCalculation(35);
+// gradeCalculation(150);
+// gradeCalculation(29);
+// gradeCalculation(-7);
+// gradeCalculation(49);
+// gradeCalculation(null);
+// gradeCalculation(64);
+// gradeCalculation(NaN);
+// gradeCalculation("91");
+// gradeCalculation("Eighty");
+
