1ce480f224d05c5a7b53c7785aa092f576bf7d53
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-20 14:36:48 +0530

    For loop Ex
    For loop Ex

---
+++ 08_forLoopEx.js
@@ -0,0 +1,45 @@
+// for(let index=17; index<=170; index=index+17){
+//     console.log(index);
+// }
+
+// for(let index=190 ; index>=19; index=index-19){
+//     console.log(index);
+// }
+
+// for(let index=8; index<=80; index=index+8)
+// {
+//     console.log(index);
+// }
+
+// var str = "JavaScript"
+// for(let index = 0; index <str.length ; index++)
+// {
+//     var char =str.charAt(index);
+//     console.log(char);
+// }
+
+// var str = "JavaScript"
+// var reverseString ="";
+// for(let index=str.length-1 ; index>=0; index=index-1)
+// {
+//     var char = str.charAt(index);
+//     console.log(char);
+//     reverseString=reverseString.concat(char);
+//     // console.log(reverseString);
+// }
+// console.log(reverseString);
+
+
+// var str1 = "JavaScript Language Of Internet"
+// for(let index=str1.length-1 ; index>=0; index=index-1)
+// {
+//     var char1= str1.charAt(index);
+//     console.log(char1);
+// }
+
+var str1 = "JavaScript Language Of Internet";
+for(let index=0 ; index<str1.length;index++)
+{
+    var char = str1.charAt(index);
+    console.log(char);
+}
