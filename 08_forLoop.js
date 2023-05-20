2269282b1ac4e0ea2e2d0006281764c19d2c1bb7
Ajaypatil1713 <ajay87855@gmail.com>
2023-05-20 14:35:57 +0530

    for loop
    for loop

--- 08_forLoop.js
+++ 08_forLoop.js
@@ -1,4 +1,13 @@
-for (let index = 0; index <= 10; index++) { // index = 11
-    console.log(index); // 0 1  2 ... 10 
+// for (let index = 0; index <= 10; index++) { // index = 11
+//     console.log(index); // 0 1  2 ... 10 
+// }
+// console.log("After loop");
+
+for(let index=17; index<=170; index+17)
+{
+    console.log(index);
 }
-console.log("After loop");
+
+for(let index=190 ; index<=19; index-19){
+    console.log(index);
+}
